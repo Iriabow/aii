3dca66132ee8ee3d9d76e6e4ad71666675f68935
Iriabow <guilderwars@gmail.com>
2019-01-13 12:44:13 +0100

    Primer commit de recomendaciones

---
+++ Recomendaciones/RSMovieLens/urls.py
@@ -0,0 +1,17 @@
+#encoding:utf-8
+
+from django.urls import path
+from django.contrib import admin
+from main import views
+
+urlpatterns = [
+    path('', views.index),
+    path('populate', views.populateDB),
+    path('loadRS', views.loadRS),
+    path('search', views.search),
+    path('recommendedFilms', views.recommendedFilms),
+    path('similarFilms', views.similarFilms),
+    path('admin/', admin.site.urls),
+]
+
+
---
+++ Recomendaciones/main/views.py
@@ -0,0 +1,94 @@
+import shelve
+from main.models import UserInformation, Film, Rating
+from main.forms import UserForm, FilmForm
+from django.shortcuts import render, get_object_or_404
+from main.recommendations import  transformPrefs, calculateSimilarItems, getRecommendedItems, topMatches
+from main.populate import populateDatabase
+
+
+
+# Funcion que carga en el diccionario Prefs todas las puntuaciones de usuarios a peliculas. Tambien carga el diccionario inverso y la matriz de similitud entre items
+# Serializa los resultados en dataRS.dat
+def loadDict():
+    Prefs={}   # matriz de usuarios y puntuaciones a cada a items
+    shelf = shelve.open("dataRS.dat")
+    ratings = Rating.objects.all()
+    for ra in ratings:
+        user = int(ra.user.id)
+        itemid = int(ra.film.id)
+        rating = float(ra.rating)
+        Prefs.setdefault(user, {})
+        Prefs[user][itemid] = rating
+    shelf['Prefs']=Prefs
+    shelf['ItemsPrefs']=transformPrefs(Prefs)
+    shelf['SimItems']=calculateSimilarItems(Prefs, n=10)
+    shelf.close()
+    
+
+
+    
+#  CONJUNTO DE VISTAS
+
+def index(request): 
+    return render(request,'index.html')
+
+def populateDB(request):
+    populateDatabase() 
+    return render(request,'populate.html')
+
+def loadRS(request):
+    loadDict()
+    return render(request,'loadRS.html')
+ 
+#APARTADO A
+def search(request):
+    if request.method=='GET':
+        form = UserForm(request.GET, request.FILES)
+        if form.is_valid():
+            idUser = form.cleaned_data['id']
+            user = get_object_or_404(UserInformation, pk=idUser)
+            return render(request,'ratedFilms.html', {'usuario':user})
+    form=UserForm()
+    return render(request,'search_user.html', {'form':form })
+
+# APARTADO B
+def recommendedFilms(request):
+    if request.method=='GET':
+        form = UserForm(request.GET, request.FILES)
+        if form.is_valid():
+            idUser = form.cleaned_data['id']
+            user = get_object_or_404(UserInformation, pk=idUser)
+            shelf = shelve.open("dataRS.dat")
+            Prefs = shelf['Prefs']
+            SimItems = shelf['SimItems']
+            shelf.close()
+            rankings = getRecommendedItems(Prefs, SimItems, int(idUser))
+            recommended = rankings[:2]
+            items = []
+            for re in recommended:
+                item = Film.objects.get(pk=re[1])
+                items.append(item)
+            return render(request,'recommendationItems.html', {'user': user, 'items': items})
+    form = UserForm()
+    return render(request,'search_user.html', {'form': form})
+
+# APARTADO C
+def similarFilms(request):
+    film = None
+    if request.method=='GET':
+        form = FilmForm(request.GET, request.FILES)
+        if form.is_valid():
+            idFilm = form.cleaned_data['id']
+            film = get_object_or_404(Film, pk=idFilm)
+            shelf = shelve.open("dataRS.dat")
+            ItemsPrefs = shelf['ItemsPrefs']
+            shelf.close()
+            recommended = topMatches(ItemsPrefs, int(idFilm),n=3)
+            items=[]
+            for re in recommended:
+                item = Film.objects.get(pk=int(re[1]))
+                items.append(item)
+            return render(request,'similarFilms.html', {'film': film,'films': items})
+    form = FilmForm()
+    return render(request,'search_film.html', {'form': form})
+
