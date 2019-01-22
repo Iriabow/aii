12019cba2bb4792ecdb1922b12784a67c58584de
McAllisterDTur <Joseph.jmlc@gmail.com>
2019-01-22 02:33:47 +0100

    Cambio en el formulario de búsqueda

--- Licoreando/licor/views.py
+++ Licoreando/licor/views.py
@@ -1,26 +1,38 @@
 from django.shortcuts import render, get_object_or_404
 
 from licor.forms import SearchForm
-from licor.models import Licor
+from licor.models import Licor, Categoria
+from whooshLicor.utils import listarPorAtributo
+
 
 def index(request): 
     return render(request,'index.html')
 
 def buscarLicor(request):
+    form=SearchForm()
+    categorias = Categoria.objects.all()
     if request.method=='POST':
         form = SearchForm(request.POST, request.FILES)
         if form.is_valid():
-            titulo = form.cleaned_data['Título']
-            precio = form.cleaned_data['Precio']
-            graduacion = form.cleaned_data['Graduación']
-            procedencia = form.cleaned_data['Procedencia']
-            categoria = form.cleaned_data['Categoría']
-            licores = Licor.objects.all()
-            form=SearchForm()
-            return render(request,'search_licor.html', {'licores':licores,'form':form })
-    form=SearchForm()
-    licores = Licor.objects.all()
-    return render(request,'search_licor.html', {'form':form,'licores':licores})
+            busqueda = form.cleaned_data["busqueda"]
+            graduacionMinima = form.cleaned_data["graduacionMinima"]
+            graduacionMaxima = form.cleaned_data["graduacionMaxima"]
+            precioMinimo = form.cleaned_data["precioMinimo"]
+            precioMaximo = form.cleaned_data["precioMaximo"]
+            print(busqueda,"+",graduacionMinima,"+",graduacionMaxima,"+",precioMinimo,"+",precioMaximo)
+#             ordenarSimilitud = form.cleaned_data["ordenarSimilitud"]
+#             ordenarPrecio = form.cleaned_data["ordenarPrecio"]
+#             ordenarTitulo = form.cleaned_data["ordenarTitulo"]
+#             ordenarGraduacion = form.cleaned_data["ordenarGraduacion"]
+#             ordenarProcedencia = form.cleaned_data["ordenarProcedencia"]
+#            categoria = form.cleaned_data["Categoría"]           
+#            licoresId = listarPorAtributo(busqueda=busqueda,categoria=[], order ="",groupDic={}, nElementosPagina=20, pagina=1)
+#            licores = getLicores(licoresId)
+            licores = []
+            return render(request,'search_licor.html', {'licores':licores,'form':form ,'categorias': categorias})
+    licoresId = listarPorAtributo()
+    licores = getLicores(licoresId)
+    return render(request,'search_licor.html', {'form':form,'licores':licores, 'categorias': categorias})
 
 def recomendacion(request):
     return render(request,'index.html')
@@ -70,3 +82,8 @@ def recomendarLicor(request):
     form = FilmForm()
     return render(request,'search_film.html', {'form': form})
 '''
+def getLicores(licoresId):
+    licores = []
+    for licor in licoresId[0]:
+        licores.append(Licor.objects.get(id=licor['id'])) 
+    return licores
